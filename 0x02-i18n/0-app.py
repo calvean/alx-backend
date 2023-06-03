727d0569116315727a1ccecb273d14ac7a4f00c7
calvean <garnetsharara@gmail.com>
2023-06-03 19:35:06 +0200

    fixes

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -5,11 +5,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def index():
     """ Serves index page """
     return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port='5000')
