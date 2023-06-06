ef56adebcac326a5a3b1b51e5499bf7e8dec8ebc
calvean <garnetsharara@gmail.com>
2023-06-06 19:45:39 +0200

    fixed pycodestyle

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -6,13 +6,16 @@ from flask_babel import Babel, gettext as _
 app = Flask(__name__)
 babel = Babel(app)
 
+
 class Config:
     """ Language and locale configs """
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
+
 app.config.from_object(Config)
+
 
 @babel.localeselector
 def get_locale():
@@ -22,12 +25,14 @@ def get_locale():
         return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
+
 @app.route('/', strict_slashes=False)
 def index():
     """ Serves index page with parametrized templates """
     return render_template('3-index.html',
                            title=_('home_title'),
                            header=_('home_header'))
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000', debug=True)
